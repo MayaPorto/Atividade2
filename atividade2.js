3621c154a8247d951be6830c7fc2722f39214b8e
MayaPorto <mayamiresporto@gmail.com>
2022-09-25 00:01:27 -0300

    atividade2

--- atividade2.js
+++ atividade2.js
@@ -1,30 +1,22 @@
 
-const litro = parseFloat(prompt('QUANTOS LITROS DESEJA?'))
+let litros =parseInt(prompt('Qual a quantidade de litros?'));
+console.log(litros);
+let tipoCombustivel = prompt('Escreva A para Alcool e G para gasolina').toUpperCase();
+console.log(tipoCombustivel);
 
-
-const mensagem = `
-Escolha a opção desejada:
-Digite 1 para Álcool
-Digite 2 para Gasolina
-`
-const combustivel = prompt(mensagem)
-
-console.log(litro)
-console.log(combustivel)
-
-const A = 1.90
-const G = 2.70
-
-if (combustivel == 1 && litro <= 25) {
- const precoFinal = (A * litro) - 0.2
- alert(`O valor que pagará pelo álcool é ${precoFinal} reais` )
-} else if (combustivel == 1 && litro > 25) {
-const precoFinal = (A * litro) - 0.4
-alert(`O valor que pagará pelo álcool é ${precoFinal} reais` )
-} else if (combustivel == 2 && litro <= 25) {
-    const precoFinal = (G * litro) - 0.3
-    alert(`O valor que pagará pela gasolina é ${precoFinal} reais` )
-} else {
-    const precoFinal = (G * litro) - 0.3
-    alert(`O valor que pagará pela gasolina é ${precoFinal} reais`)
-}
+if (tipoCombustivel == "A" && tipoCombustivel <= 25) {
+    let litrosVendidos = litros * 1.90;
+    let desconto = litros - (litrosVendidos * 0.02);
+   alert(`O valor a ser pago pelo cliente pelo Alcool com desconto é: R$ ${desconto}`)
+} else if (tipoCombustivel == "A" && tipoCombustivel >= 25) {
+    let litrosVendidos = litros * 1.90;
+    let desconto = litros - (litrosVendidos * 0.04);
+    alert(`O valor a ser pago pelo cliente pelo Alcool com desconto é: R$ ${desconto}`)
+} else if (tipoCombustivel == "G" && tipoCombustivel <= 25) {
+    let litrosVendidos = litros * 2.70;
+    let desconto = litros - (litrosVendidos * 0.03);
+    alert(`O valor a ser pago pelo cliente pela Gasolina com desconto é: R$ ${desconto}`)
+}else {
+    let litrosVendidos = litros * 2.70;
+    let desconto = litros - (litrosVendidos * 0.05);
+    alert(`O valor a ser pago pelo cliente pela Gasolina com desconto é: R$ ${desconto}`)
